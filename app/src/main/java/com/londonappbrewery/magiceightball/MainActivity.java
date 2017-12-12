ca3ec8e8edf69c6b879e84e0a7abf7810ce83d0d
pncontemplacion <paulbcon@hotmail.com>
2017-12-12 08:51:05 -0500

    Project Completed!

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final ImageView ballDisplay = (ImageView) findViewById(R.id.image_eightBall);
+        final ImageView ballDisplay = findViewById(R.id.image_eightBall);
 
         final int[] ballArray = {
                 R.drawable.ball1,
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
                 R.drawable.ball5
         };
 
-        Button myButton = (Button) findViewById(R.id.askButton);
+        Button myButton = findViewById(R.id.askButton);
 
         myButton.setOnClickListener(new View.OnClickListener() {
             @Override
